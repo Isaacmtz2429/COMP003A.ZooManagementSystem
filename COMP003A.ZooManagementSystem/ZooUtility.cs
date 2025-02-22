a4fc11e8b7d75a9117e1707fdd03b915e901da9d
Isaac Martinez <isaacmartinez2429@gmail.com>
2025-02-22 15:23:30 -0800

    created ZooUtility class

---
+++ COMP003A.ZooManagementSystem/ZooUtility.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.ZooManagementSystem
+{
+    public static class ZooUtility
+    {
+        public static void DescribeAnimal(string name)
+        {
+            Console.WriteLine($"Animal Name: {name}");
+        }
+        public static void DescribeAnimal(string name, string species)
+        {
+            Console.WriteLine($"Animal Name: {name}, Species: {species}");
+        }
+        public static void DescribeAnimal(string name, string species, int age)
+        {
+            Console.WriteLine($"Animal Name: {name}, Species: {species}, Age: {age} years old");
+        }
+    }
+}
