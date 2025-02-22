6996c0d33320feda96f0c5c6da9d5e8053dbde74
Isaac Martinez <isaacmartinez2429@gmail.com>
2025-02-22 14:47:27 -0800

    class animal created

--- COMP003A.ZooManagementSystem/Animal.cs
+++ COMP003A.ZooManagementSystem/Animal.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace COMP003A.ZooManagementSystem
 {
-    internal class Animal
+    public abstract class Animal
     {
+        private string _name;
+        private string _species;
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if(string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Name cannot be null or empty.");
+                _name = value;
+            }
+        }
+
+        public string Species
+        {
+            get => _species;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Species cannot be null or empty.");
+                _species = value;
+            }
+        }
+
+        public Animal( string name, string species)
+        {
+            Name = name;
+            Species = species;
+        }
     }
 }
