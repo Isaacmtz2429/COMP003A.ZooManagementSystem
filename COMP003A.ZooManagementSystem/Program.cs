c7c25407fbd6ae6aac7a86aa66d4c9b71df3de3b
Isaac Martinez <isaacmartinez2429@gmail.com>
2025-02-22 15:52:07 -0800

    created the list menu

--- COMP003A.ZooManagementSystem/Program.cs
+++ COMP003A.ZooManagementSystem/Program.cs
@@ -8,9 +8,19 @@ namespace COMP003A.ZooManagementSystem
 {
     internal class Program
     {
+        static List<Animal> animals = new List<Animal>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Console.WriteLine("\nWelcome to the Zoo Managment System!");
+                Console.WriteLine("Please choose an option:");
+                Console.WriteLine("1. Add a Lion");
+                Console.WriteLine("2. Add a Parrot");
+                Console.WriteLine("3. Display All Animals");
+                Console.WriteLine("4. Describe an Animal");
+                Console.WriteLine("5. Exit");
+            }
         }
     }
 }
